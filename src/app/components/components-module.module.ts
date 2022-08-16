6837223968d571b505dd669362b62b2b552787d6
Mr Internauta <feldjesus@gmail.com>
2022-08-16 15:58:04 -0500

    :art: Create Cakes project

---
+++ src/app/components/components-module.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { PaymentMethodComponent } from './payment-method/payment-method.component';
+
+@NgModule({
+  declarations: [DropdownComponent, PaymentMethodComponent],
+  imports: [CommonModule],
+  exports: [DropdownComponent, PaymentMethodComponent],
+})
+export class ComponentsModuleModule {}
